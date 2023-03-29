f844f1de596b0ef46cb55bb9789ec6df2e92a0e7
adamazai0@gmail.com <adamazai0@gmail.com>
2023-03-29 16:13:13 -0700

    completed post endpoint for forum with error handling

--- green_creme/queries/forums.py
+++ green_creme/queries/forums.py
@@ -1,26 +1,51 @@
-# from pydantic import BaseModel
-# from typing import Optional
-# from queries.pool import pool
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+from queries.pool import pool
 
 
-# class ThreadIn(BaseModel):
-#     title: str
-#     body: str
-#     image: Optional[str]
+class Error(BaseModel):
+    message: str
 
 
-# class ThreadRepository:
-#     def create(thread: ThreadIn):
-#         with pool.connection() as conn:
-#             with conn.cursor() as db:
-#                 result = db.execute(
-#                     """
-#                     insert into forum
-#                         (title, body, image)
-#                     values
-#                         (%s,%s,%s)
-#                     returning id;
-#                     """,
-#                     [thread.title, thread.body, thread.image],
-#                 )
-#                 print(result)
+class ThreadIn(BaseModel):
+    title: str
+    body: str
+    image: Optional[str]
+    author_id: int
+
+
+class ThreadOut(BaseModel):
+    id: int
+    title: str
+    body: str
+    image: Optional[str]
+    author_id: int
+    created_on: datetime = datetime.now()
+
+
+class ThreadRepository:
+    def create(self, thread: ThreadIn) -> ThreadOut:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
+                    insert into forum
+                        (title, body, image, author_id)
+                    values
+                        (%s,%s,%s,%s)
+                    returning id, created_on;
+                    """,
+                    [
+                        thread.title,
+                        thread.body,
+                        thread.image,
+                        thread.author_id,
+                    ],
+                )
+                id = result.fetchone()[0]
+                return self.forum_in_to_out(id, thread)
+
+    def forum_in_to_out(self, id: int, thread: ThreadIn):
+        old_data = thread.dict()
+        return ThreadOut(id=id, **old_data)
--- green_creme/routers/forum.py
+++ green_creme/routers/forum.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter
-# from queries.forums import ThreadIn
+from fastapi import APIRouter, Depends, Response, HTTPException
+from queries.forums import Error, ThreadIn, ThreadRepository, ThreadOut
+from typing import Union
 
 router = APIRouter()
 
 
-# @router.post("/forum/")
-# def create_thread(thread: ThreadIn):
-#     print("thread", thread)
-#     return thread
+@router.post("/forum/", response_model=Union[ThreadOut, Error])
+def create_thread(
+    thread: ThreadIn, response: Response, repo: ThreadRepository = Depends()
+):
+    if thread.title == "" or thread.body == "":
+        response.status_code = 400
+        return {"message": "Could not create forum"}
+    try:
+        return repo.create(thread)
+    except:
+        response.status_code = 400
+        return {"message": "Could not create forum"}
