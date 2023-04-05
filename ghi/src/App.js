3261c9e24ab770c2d8c9f8083d21bf31dd1c4135
Adrienne Shayne Buac <asmbuac@gmail.com>
2023-04-05 22:38:22 +0000

    Created frontend signup form and added route for signup page #1

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,24 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
-import LoginForm from './LoginForm.js';
-import BlogList from './BlogList.js';
-import { useGetTokenQuery } from './store/authApi';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./LoginForm.js";
+import Signup from "./Signup.js";
+import BlogList from "./BlogList.js";
+import { useGetTokenQuery } from "./store/authApi";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const { data } = useGetTokenQuery();
 
   return (
     <>
-      { data ?
-      <Routes>
-        <Route path='/blogs' element={<BlogList />} />
-      </Routes>
-      :
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-      </Routes>
-      }
+      {data ? (
+        <Routes>
+          <Route path="/blogs" element={<BlogList />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      )}
       <ToastContainer />
     </>
   );
