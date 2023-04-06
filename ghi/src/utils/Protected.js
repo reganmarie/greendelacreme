3c3a815d7ae57099a77736ebcc2aeecb1f1d664e
adamazai0@gmail.com <adamazai0@gmail.com>
2023-04-06 13:58:52 -0700

    if conditional statement for token

--- ghi/src/utils/Protected.js
+++ ghi/src/utils/Protected.js
@@ -3,7 +3,11 @@ import { Navigate, Outlet } from "react-router-dom";
 
 
 const Protected = ({ token }) => {
+    if (token === undefined){
+    return null
+  }
   return (
+
     token === null ?
       <Navigate to="/" replace />
       :
