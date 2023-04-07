e6da7a7cd14e1f32d6c8d20bebc662dbef7f8691
Shayne <asmbuac@gmail.com>
2023-04-06 18:11:07 -0700

    Added cute icons for the fake like and comment buttons

--- ghi/src/blogs/components/BlogPost.js
+++ ghi/src/blogs/components/BlogPost.js
@@ -18,9 +18,9 @@ export default function BlogPost({ username, avatar, createdOnDate, createOnTime
             <img src={avatar} alt="avatar" className="h-8 w-8 rounded-full bg-lime-400" />
             <div className="text-lg font-bold text-slate-700"> {username}</div>
           </div>
-          <div className="flex flex-col text-end space-x-8">
-            <div className="text-xs text-neutral-500">{createdOnDate}</div>
-            <div className="text-xs text-neutral-500">{createOnTime}</div>
+          <div className="flex flex-col text-end space-x-8 text-secondary-200">
+            <div className="text-xs">{createdOnDate}</div>
+            <div className="text-xs">{createOnTime}</div>
           </div>
         </div>
 
@@ -30,18 +30,14 @@ export default function BlogPost({ username, avatar, createdOnDate, createOnTime
         </div>
 
         <div>
-          <div className="flex items-center space-x-5 text-slate-500">
-            <div className="flex cursor-pointer items-center transition hover:text-slate-600">
-              <svg xmlns="http://www.w3.org/2000/svg" className="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
-              </svg>
+          <div className="flex items-center space-x-5 text-secondary-200">
+            <div className="flex cursor-pointer items-center transition hover:text-darkgreen">
+              <img src="../images/hand-holding-up-a-flower.png" className="mr-1.5 h-5 w-5" fill="none" />
               <span>4</span>
             </div>
             <div className="flex space-x-4 md:space-x-8">
-              <div className="flex cursor-pointer items-center transition hover:text-slate-600">
-                <svg xmlns="http://www.w3.org/2000/svg" className="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                </svg>
+              <div className="flex cursor-pointer items-center transition hover:text-darkgreen">
+                <img src="../images/plant-comment-icon.png" className="mr-1.5 h-5 w-5" fill="none" />
                 <span>125</span>
               </div>
             </div>
--- ghi/tailwind.config.js
+++ ghi/tailwind.config.js
@@ -6,10 +6,11 @@ module.exports = {
     extend: {
       colors: {
         primary: {"50":"#ecfdf5","100":"#d1fae5","200":"#a7f3d0","300":"#6ee7b7","400":"#34d399","500":"#10b981","600":"#059669","700":"#047857","800":"#065f46","900":"#064e3b"},
-        secondary: "#b5c1ac",
+        secondary: {"100":"#b5c1ac", "200":"#909B93"},
         lemon: "#ffdfac",
         tertiary: "#e1e5e8",
         darkgreen: "#2f4034",
+
       },
     },
   plugins: [],
