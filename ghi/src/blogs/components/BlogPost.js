330befe5a95321ad4bf009564e67f235e8d7de13
Shayne <asmbuac@gmail.com>
2023-04-06 17:03:50 -0700

    Fixed spacing between fake like and comment buttons

--- ghi/src/blogs/components/BlogPost.js
+++ ghi/src/blogs/components/BlogPost.js
@@ -30,7 +30,13 @@ export default function BlogPost({ username, avatar, createdOnDate, createOnTime
         </div>
 
         <div>
-          <div className="flex items-center justify-between text-slate-500">
+          <div className="flex items-center space-x-5 text-slate-500">
+            <div className="flex cursor-pointer items-center transition hover:text-slate-600">
+              <svg xmlns="http://www.w3.org/2000/svg" className="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
+              </svg>
+              <span>4</span>
+            </div>
             <div className="flex space-x-4 md:space-x-8">
               <div className="flex cursor-pointer items-center transition hover:text-slate-600">
                 <svg xmlns="http://www.w3.org/2000/svg" className="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -38,12 +44,6 @@ export default function BlogPost({ username, avatar, createdOnDate, createOnTime
                 </svg>
                 <span>125</span>
               </div>
-            </div>
-            <div className="flex cursor-pointer items-center transition hover:text-slate-600">
-              <svg xmlns="http://www.w3.org/2000/svg" className="mr-1.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
-              </svg>
-              <span>4</span>
             </div>
           </div>
         </div>
