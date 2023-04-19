e8fb134d4069d546966242bfbfa5878bc7f9911d
adamazai0@gmail.com <adamazai0@gmail.com>
2023-04-19 16:24:38 -0700

    Moved Browser Router to App.js for deployment

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -6,7 +6,7 @@ import MainPage from './MainPage.js';
 import Signup from './Signup.js';
 import Protected from './utils/Protected.js';
 import ForumDetail from './forums/ForumDetail.js';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { useGetTokenQuery } from './store/authApi';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,14 +16,14 @@ function App() {
   const { data } = useGetTokenQuery();
 
   const domain = /https:\/\/[^/]+/;
-  const basename = proces.env.PUBLIC_URL.replace(domain, '');
+  const basename = process.env.PUBLIC_URL.replace(domain, '');
 
   if (data === undefined) {
     return null;
   }
 
   return (
-    <>
+    <BrowserRouter basename={basename}>
       <Nav isLoggedIn={data} />
       <Routes>
         <Route path='*' element={<PageNotFound/>} />
@@ -37,7 +37,7 @@ function App() {
         <Route path="/signup" element={<Signup token={data} />} />
       </Routes>
       <ToastContainer position="bottom-right" />
-    </>
+    </BrowserRouter>
   );
 }
 
