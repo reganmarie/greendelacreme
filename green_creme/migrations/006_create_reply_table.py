cd09551f3f67b3a55c75df4b9a0c08d459ed082e
adamazai0@gmail.com <adamazai0@gmail.com>
2023-04-20 15:51:19 -0700

    created new migration for forum replies

---
+++ green_creme/migrations/006_create_reply_table.py
@@ -0,0 +1,25 @@
+steps = [
+    [
+        """
+        CREATE table reply (
+            id serial primary key not null,
+            author_id int not null references accounts (id),
+            forum_id int not null references forum (id),
+            answer text not null,
+            image varchar(400),
+            rating smallint not null default 0 ,
+            created_on timestamp not null default current_timestamp,
+            constraint reply_author_fk
+                foreign key (author_id) references accounts (id)
+                on delete cascade,
+            constraint reply_forum_fk
+                foreign key (forum_id) references forum (id)
+                on delete cascade
+
+        );
+        """,
+        """
+        DROP table reply;
+        """,
+    ]
+]
